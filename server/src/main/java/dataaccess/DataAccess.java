5897d3b92cd6950bb332c916ae6f893f826ec5b9
mrmintmango <ruben.matos.imp@gmail.com>
2024-10-16 19:56:56 -0600

    Fixed the three record classes and made maps to store them in the DataAccess class. I made a clear function and create methods for the three data types. I hope using maps is the right way to go for storing.

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,28 +1,42 @@
 package dataaccess;
 
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DataAccess {
-    Map<String, >
-    //Map<String, AuthData> authDataMap = new HashMap<String, AuthData>();
-    //Map<String, GameData> gameDataMap = new HashMap<String, GameData>();
-    //Map<String, UserData> userDataMap = new HashMap<String, UserData>();
-
+    Map<String, AuthData> authDataMap = new HashMap<>();
+    Map<String, GameData> gameDataMap = new HashMap<>();
+    Map<String, UserData> userDataMap = new HashMap<>();
     public DataAccess() {
 
     }
 
     void clear() {
-        userDataMap.clear();
         authDataMap.clear();
         gameDataMap.clear();
+        userDataMap.clear();
     }
 
+    // User Data Methods:
     void createUser(String username, String password, String email) {
-        userDataMap.put(username, new UserData());
+        userDataMap.put(username, new UserData(username, password, email));
+    }
+
+    // Game Data Methods:
+    void createGame(int gameID, String white, String black, String gameName, ChessGame game) {
+        gameDataMap.put(gameName, new GameData(gameID, white, black, gameName, game));
+    }
+
+    // Auth Data Methods:
+    void createAuth(String authToken, String username) {
+        authDataMap.put(username, new AuthData(authToken, username));
     }
 
 }
