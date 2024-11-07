40f2415e6baf73233206f6ad59a90453d3080028
mrmintmango <ruben.matos.imp@gmail.com>
2024-11-07 15:22:10 -0700

    made lists

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -1,4 +1,20 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ChessBoard {
+        ArrayList<String> letters;
+        ArrayList<Integer> numbers;
+    public ChessBoard(){
+        letters = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
+        numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+
+        //create the board time!! woop woop!!
+    }
+
+    public void createBoarder() {
+
+    }
+
 }
