a101d6bcd1dc40ffb2b9c8c8a8dc6ae9e061dea8
mrmintmango <ruben.matos.imp@gmail.com>
2024-11-07 18:01:54 -0700

    starting steps on making my chess board look good

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -2,19 +2,76 @@ package ui;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.io.PrintStream;
 
-public class ChessBoard {
-        ArrayList<String> letters;
-        ArrayList<Integer> numbers;
+import static java.lang.System.out;
+
+public class ChessBoard extends EscapeSequences {
+    ArrayList<String> letters;
+    ArrayList<Integer> numbers;
+
+    //Board dimensions
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+
     public ChessBoard(){
         letters = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
         numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
 
-        //create the board time!! woop woop!!
     }
 
     public void createBoarder() {
+        drawHeader(out, true);
+        out.println();
 
+        //Chess Board time
+
+
+        drawHeader(out, false);
+        out.println();
+    }
+
+    public void drawHeader(PrintStream out, boolean topBottom) {
+        setWhite(out);
+        out.print(SET_TEXT_COLOR_BLACK);
+
+        //top = true, bottom = false
+        if(topBottom){
+            out.print("   ");
+            for (String letter : letters) {
+                out.print(" " + letter + " ");
+            }
+            out.print("   ");
+            setBlack(out);
+        }
+        else {
+            out.print("   ");
+            for (int i = letters.size()-1; i >= 0; i--) {
+                out.print(" " + letters.get(i) + " ");
+            }
+            out.print("   ");
+            setBlack(out);
+        }
+
+    }
+
+    public void drawBoard(boolean bw){
+        //white = true, black = false
+
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setRed(PrintStream out) {
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
     }
 
 }
