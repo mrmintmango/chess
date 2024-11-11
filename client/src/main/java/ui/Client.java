39133ca57ba6e2eae9046ec42c2cd4ec911d9e05
mrmintmango <ruben.matos.imp@gmail.com>
2024-11-11 10:24:19 -0700

    The full ui of the menu has been implemented. Now I need to connect its functionality to the server facade.

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -8,15 +8,30 @@ import static java.lang.System.out;
 public class Client {
 
     public Client(){
+        out.print("Welcome to 240 Chess. Type the corresponding number to get started");
         Scanner scanner = new Scanner(System.in);
         loggedOutMenu();
         menuCalculatorOut(scanner);
     }
 
+//    public void printMenu1(Scanner scan) {
+//        out.println();
+//        out.println();
+//        loggedOutMenu();
+//        menuCalculatorOut(scan);
+//    }
+//
+//    public void printMenu2(Scanner scan) {
+//        out.println();
+//        out.println();
+//        loggedInMenu();
+//        menuCalculatorIn(scan);
+//    }
+
+
     public void loggedOutMenu() {
-        out.print(" Welcome to 240 Chess. Type the corresponding number to get started");
-        out.println("\n[LOGGED OUT]");
-        out.println("\n1. Register");
+        out.println("\n\n[LOGGED OUT]");
+        out.println("1. Register");
         out.println("2. Login");
         out.println("3. Quit");
         out.println("4. Help");
@@ -24,7 +39,7 @@ public class Client {
 
     public void loggedInMenu() {
         out.println("[LOGGED IN]");
-        out.println("/n 1. Help");
+        out.println("1. Help");
         out.println("2. Logout");
         out.println("3. Create Game");
         out.println("4. List Games");
@@ -42,14 +57,22 @@ public class Client {
             out.println("EMAIL: ");
             String email = scan.nextLine();
             // create a register request thing for the server facade I think
+            out.println("You've been registered!");
+
+            menuCalculatorOut(scan);
         }
         else if(Objects.equals(input, "2")) { //Login Input
             out.println("USERNAME: ");
             String username = scan.nextLine();
             out.println("PASSWORD: ");
             String password = scan.nextLine();
+
+            //First check if that players username and password is correct
+            loggedInMenu();
+            menuCalculatorIn(scan);
         }
         else if(Objects.equals(input, "3")) { //Quits the program
+            out.println("Thanks for playing!");
             System.exit(0);
         }
         else if(Objects.equals(input, "4")) { //Help output
@@ -57,12 +80,61 @@ public class Client {
             out.println("2- Login as an existing user");
             out.println("3- Exit the program");
             out.println("4- Displays this help menu again");
+            menuCalculatorOut(scan);
+        }
+        else {
+            out.println("... please input a valid option:");
             out.println();
-            out.println();
+            menuCalculatorOut(scan);
+        }
+
+    }
+
+    public void menuCalculatorIn(Scanner scan) {
+        String input = scan.nextLine();
+        if (Objects.equals(input, "1")){ //Help Output
+            out.println("1- Displays this help menu again");
+            out.println("2- Logout current user");
+            out.println("3- Create a new chess game");
+            out.println("4- List all of the existing games");
+            out.println("5- Play one of the existing games");
+            out.println("6- Spectate a game that is being played");
+            menuCalculatorIn(scan);
+        }
+        else if(Objects.equals(input, "2")) { //Logout
+            //logout request for server goes here
+            out.println("You've been logged out.");
             loggedOutMenu();
             menuCalculatorOut(scan);
         }
-    }
+        else if(Objects.equals(input, "3")) { //Create a new game
+            out.println("Please name the new game: ");
+            String gameName = scan.nextLine();
+            // create game code goes here
+            out.println("Game has been created");
+            menuCalculatorIn(scan);
+        }
+        else if(Objects.equals(input, "4")) { //List Games
+            out.println("List of existing games: ");
+            //Server list game request and printing
+            menuCalculatorIn(scan);
+        }
+        else if(Objects.equals(input, "5")) { //Play a game
+            out.println("I don't have this functionality yet, \nbut check back in the next update");
+            menuCalculatorIn(scan);
+        }
+        else if(Objects.equals(input, "6")) { //Observe a game
+            out.println("I don't have this functionality yet, \nbut check back in the next update");
+            out.println("\nIn the meantime, here is the chessboard you'd like to see: ");
 
-    public void menuCalculatorIn() {}
+            //output the given chessboard.
+
+            menuCalculatorIn(scan);
+        }
+        else {
+            out.println("... please input a valid option:");
+            out.println();
+            menuCalculatorIn(scan);
+        }
+    }
 }
