e0be0730f1571d59e2e87d6c4e35da5ee6a23338
mrmintmango <ruben.matos.imp@gmail.com>
2024-11-12 20:07:46 -0700

    Got started on writing my unit tests. Still got a lot to do but after that then I'm done.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,16 +2,20 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
+
+import java.io.IOException;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    ServerFacade serverFacade = new ServerFacade("http://localhost:8080");
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -25,5 +29,21 @@ public class ServerFacadeTests {
     public void sampleTest() {
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void registerPassTest() throws IOException {
+        String answer = serverFacade.register("user1", "pass", "email");
+
+        Assertions.assertEquals(answer, "GOOD");
+    }
+
+    @Test
+    public void registerFailTest() throws IOException {
+        String answer = serverFacade.register("user1", null, "email");
+
+        Assertions.assertNotEquals(answer, "GOOD");
+    }
+
+
 
 }
