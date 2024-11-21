49b68989f7493774099bef3ebb5395af8ca16219
mrmintmango <ruben.matos.imp@gmail.com>
2024-11-20 23:22:58 -0700

    Made the command deserializer

--- server/src/main/java/handler/WebsocketHandler.java
+++ server/src/main/java/handler/WebsocketHandler.java
@@ -4,7 +4,7 @@ import com.google.gson.*;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
-import websocket.commands.UserGameCommand;
+import websocket.commands.*;
 
 import java.lang.reflect.Type;
 
@@ -34,16 +34,18 @@ public class WebsocketHandler {
 
     private static class CommandDeserializer implements JsonDeserializer<UserGameCommand> {
         @Override
-        public UserGameCommand deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        public UserGameCommand deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
             JsonObject jsonObject = jsonElement.getAsJsonObject();
 
             String typeString = jsonObject.get("type").getAsString();
             UserGameCommand.CommandType commandType = UserGameCommand.CommandType.valueOf(typeString);
 
             return switch(commandType) {
-                case CONNECT -> context.deserialize(jsonElement, );
-                //case Car -> context.deserialize(jsonElement, Car.class);
-                //case Truck -> context.deserialize(jsonElement, Truck.class);
+                case CONNECT -> context.deserialize(jsonElement, ConnectCommand.class);
+                case MAKE_MOVE -> context.deserialize(jsonElement, MakeMoveCommand.class);
+                case LEAVE -> context.deserialize(jsonElement, LeaveCommand.class);
+                case RESIGN -> context.deserialize(jsonElement, ResignCommand.class);
+                case null, default -> throw new JsonIOException("Invalid Command Type");
             };
         }
     }
